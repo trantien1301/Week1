9196328c88a25ff2969ca49214f09a8ea7cb93bf
Trantien1301 <trantienn1301@gmail.com>
2025-03-25 18:30:13 +0700

    Commit1

--- ex5.cpp
+++ ex5.cpp
@@ -14,12 +14,18 @@ bool isSorted(int arr[], int n)
 }
 int main()
 {
-	int a[] = { 0, 3, 6, 8 ,23 };
-	int n = sizeof(a) / sizeof(a[0]);
+	int n;
+	cin >> n;
+	int* a = new int[n];
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a[i];
+	}
 	if (isSorted(a, n))
 		cout << "The array is sorted.\n";
 	else
 		cout << "The array is NOT sorted.\n";
+	delete[] a;
 	return 0;
 
 }
